9f1926a6bd510fd1e923e89380924306ceb735dd
GalamBOSS <galambosadam2006@gmail.com>
2026-03-30 21:50:44 +0200

    error fix

--- app/Http/Controllers/ServicelogController.php
+++ app/Http/Controllers/ServicelogController.php
@@ -8,11 +8,6 @@ use Illuminate\Support\Facades\DB;
 
 class ServicelogController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
     public function index()
     {
         $userId = Auth::id();
